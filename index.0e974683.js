d5d5f5b368a8c1045e1623515adc020b3ce78863
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-01-17 00:46:47 +0000

    Deploying to gh-pages from @ Natoilad/goit-js-hw-11@4c6fc88cdb4abdba80030992b4a42878f47ebe2c 🚀

--- index.0e974683.js
+++
@@ -1,2 +0,0 @@
-(async()=>{try{const a=await fetch("https://pixabay.com/api/?key=32891390-a52999f78b5dd379dfcc20192&q=cat&image_type=photo&orientation=horizontal&safesearch=true"),o=await a.json();console.log(o)}catch(a){console.log(a.message)}})();
-//# sourceMappingURL=index.0e974683.js.map
